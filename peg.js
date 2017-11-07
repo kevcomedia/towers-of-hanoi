bce7010d64ca648dda6b89014301d38e5ee373a4
Kev Comedia <kevcomedia@gmail.com>
2017-11-07 11:34:48 +0800

    Write code for peg view

---
+++ peg.js
@@ -0,0 +1,24 @@
+function pegComponent() {
+  const peg = document.createElement('div');
+  peg.classList.add('peg');
+
+  return {
+    initialize(diskCount) {
+      for (let i = 0; i < diskCount; i++) {
+        const disk = document.createElement('div');
+        disk.classList.add('disk', `disk-${i}`);
+        peg.appendChild(disk);
+      }
+    },
+    pushDisk(disk) {
+      peg.insertBefore(disk, peg.firstChild);
+    },
+    popDisk() {
+      if (!peg.firstChild) throw new Error('No more disks');
+      return peg.removeChild(peg.firstChild);
+    },
+    attachTo(el) {
+      el.appendChild(peg);
+    },
+  };
+}
