6ec97c4f29930e5701cd815c21b770b8964c3500
Kev Comedia <kevcomedia@gmail.com>
2017-11-09 13:53:45 +0800

    Rename pegs

--- main.js
+++ main.js
@@ -4,19 +4,19 @@ const $diskCount = document.querySelector('#disks');
 let isRunning = false;
 
 const pegs = {
-  start: pegComponent(),
+  left: pegComponent(),
   middle: pegComponent(),
-  end: pegComponent(),
+  right: pegComponent(),
 };
 
 for (const p in pegs) {
   pegs[p].attachTo($towers);
 }
 
-pegs.start.initialize(5);
+pegs.left.initialize(5);
 
 $diskCount.addEventListener('change', function() {
   if (isRunning) return;
 
-  pegs.start.initialize(this.value);
+  pegs.left.initialize(this.value);
 });
